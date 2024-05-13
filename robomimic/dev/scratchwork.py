1c8c6ddc406cf312ff1df976bb3180e64d7f5205
Nadun Ranawaka Arachchige <nadun.kesara@gmail.com>
2024-05-12 23:11:01 -0400

    Added sbatch for robomimic lift

--- robomimic/dev/scratchwork.py
+++ robomimic/dev/scratchwork.py
@@ -3,5 +3,7 @@ import torch
 import nexusformat.nexus as nx
 
 demo_fn = "/media/nadun/Data/phd_project/robomimic/datasets/lift/ph/all_obs_v141.hdf5"
+demo_file = nx.nxload(demo_fn)
+print(demo_file.tree)
 
 print()
