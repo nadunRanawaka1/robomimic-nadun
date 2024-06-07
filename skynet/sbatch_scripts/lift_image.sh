5cf51d3f5b2229dd269db8b91f69841ff7ca7dff
Nadun Kesara Ranawaka Arachchige <nkra3@sky1.cc.gatech.edu>
2024-06-07 14:21:15 -0400

    Added configs for new experiments

--- skynet/sbatch_scripts/lift_image.sh
+++ skynet/sbatch_scripts/lift_image.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=robomimic_lift_image
-#SBATCH --output=/coc/flash7/nkra3/logs/sbatch_out/robomimic_lift_image.out
-#SBATCH --error=/coc/flash7/nkra3/logs/sbatch_err/robomimic_lift_image.err
-#SBATCH --partition=rl2-lab
-#SBATCH --time 96:00:00
+#SBATCH --job-name=robomimic_lift_image_bc_rnn_open_loop
+#SBATCH --output=/coc/flash7/nkra3/logs/sbatch_out/phd_project/robomimic_lift_image_bc_rnn_open_loop.out
+#SBATCH --error=/coc/flash7/nkra3/logs/sbatch_err/phd_project/robomimic_lift_image_bc_rnn_open_loop.err
+#SBATCH --partition=overcap
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
 #SBATCH --gpus-per-node="a40:1"
 #SBATCH --exclude="clippy"
-#SBATCH --qos="long"
-#SBATCH --mem-per-gpu=60G
+#SBATCH --exclude="chappie"
+#SBATCH --mem-per-gpu=80G
 
 export PYTHONUNBUFFERED=TRUE
 source ~/.bashrc
@@ -21,5 +20,5 @@ conda activate robomimic-dev
 
 cd /nethome/nkra3/flash7/phd_project/robomimic-nadun/robomimic
 
-srun -u python -u scripts/train.py --config=/nethome/nkra3/flash7/phd_project/robomimic-nadun/skynet/configs/lift_bc_rnn_image.json --dataset=/nethome/nkra3/flash7/phd_project/robomimic-nadun/datasets/lift/ph/all_obs_v141.hdf5
+srun -u python -u scripts/train.py --config=/nethome/nkra3/flash7/phd_project/robomimic-nadun/skynet/configs/lift_bc_rnn_image_open_loop.json 
 
