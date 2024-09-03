e1e99940aa46ff7b7d65386c760e981cac79c8e9
Yuhang <yuhang.he@gtri.gatech.edu>
2024-09-03 15:08:44 -0400

    lift_diff_joint_actions policy

--- robomimic/scripts/extract_obs_from_raw_datasets_v2.sh
+++ robomimic/scripts/extract_obs_from_raw_datasets_v2.sh
@@ -19,32 +19,32 @@ echo "Using base dataset directory: $BASE_DATASET_DIR"
 
 # lift - ph
 
-#python dataset_states_to_obs.py --done_mode 2 \
-#--dataset $BASE_DATASET_DIR/lift/ph/demo_v141.hdf5 \
-#--output_name all_obs_v141.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
+python dataset_states_to_obs.py --done_mode 2 \
+--dataset $BASE_DATASET_DIR/lift/ph/demo_v141.hdf5 \
+--output_name all_obs_v141.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
 
 # can - ph
 
-#python dataset_states_to_obs.py --done_mode 2 \
-#--dataset $BASE_DATASET_DIR/can/ph/demo_v141.hdf5 \
-#--output_name all_obs_v141.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
+python dataset_states_to_obs.py --done_mode 2 \
+--dataset $BASE_DATASET_DIR/can/ph/demo_v141.hdf5 \
+--output_name all_obs_v141.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
 
 # square - ph
 
-python dataset_states_to_obs.py --done_mode 2 \
---dataset $BASE_DATASET_DIR/square/ph/demo_v141.hdf5 \
---output_name all_obs_new.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
+# python dataset_states_to_obs.py --done_mode 2 \
+# --dataset $BASE_DATASET_DIR/square/ph/demo_v141.hdf5 \
+# --output_name all_obs_v141.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
 
 # transport - ph
 #
-#python dataset_states_to_obs.py --done_mode 2 \
-#--dataset $BASE_DATASET_DIR/transport/ph/demo_v141.hdf5 \
-#--output_name all_obs_v141.hdf5 --camera_names shouldercamera0 shouldercamera1 robot0_eye_in_hand robot1_eye_in_hand --camera_height 84 --camera_width 84
+python dataset_states_to_obs.py --done_mode 2 \
+--dataset $BASE_DATASET_DIR/transport/ph/demo_v141.hdf5 \
+--output_name all_obs_v141.hdf5 --camera_names agentview robot0_eye_in_hand --camera_height 84 --camera_width 84
 
 # tool hang - ph
 #
-#python dataset_states_to_obs.py --done_mode 2 \
-#--dataset $BASE_DATASET_DIR/tool_hang/ph/demo_v141.hdf5 \
-#--output_name all_obs_v141.hdf5 --camera_names sideview robot0_eye_in_hand --camera_height 240 --camera_width 240
+python dataset_states_to_obs.py --done_mode 2 \
+--dataset $BASE_DATASET_DIR/tool_hang/ph/demo_v141.hdf5 \
+--output_name all_obs_v141.hdf5 --camera_names sideview robot0_eye_in_hand --camera_height 84 --camera_width 84
 #
 
