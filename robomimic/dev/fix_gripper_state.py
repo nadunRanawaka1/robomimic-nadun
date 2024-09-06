a81420ff565063a73e6292aa4fb845e09121a652
Nadun Kesara Ranawaka Arachchige <nkra3@sky1.cc.gatech.edu>
2024-09-06 00:59:16 -0400

    Started act models with pick cube

--- robomimic/dev/fix_gripper_state.py
+++ robomimic/dev/fix_gripper_state.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import nexusformat.nexus as nx
 
-demo_fn = "/nethome/nkra3/flash7/ATRP_AC_Learning/datasets/speed_up_demos/demo_place_bolt_in_hole.hdf5"
+demo_fn = "/nethome/nkra3/flash7/ATRP_AC_Learning/datasets/speed_up_demos/demo_pick_cube_realsense.hdf5"
 demo_file = h5py.File(demo_fn, 'a')
 
 dataset_grp = demo_file['data']
@@ -12,6 +12,8 @@ for demo in dataset_grp:
     demo = dataset_grp[demo]
     gripper_state = demo['obs/gripper_state'][:]
     gripper_pos = gripper_state[..., np.newaxis]
+    if "gripper_pos" in demo['obs']:
+        del demo['obs/gripper_pos']
     demo.create_dataset('obs/gripper_pos', data=np.array(gripper_pos))
 
 demo_file.close()
