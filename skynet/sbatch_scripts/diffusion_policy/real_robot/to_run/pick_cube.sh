ba7e02712d152b3c865526b2b9ff4588480cb600
Nadun Kesara Ranawaka Arachchige <nkra3@sky1.cc.gatech.edu>
2024-09-08 16:42:19 -0400

    Ran new real robot models

--- skynet/sbatch_scripts/diffusion_policy/real_robot/to_run/pick_cube.sh
+++ skynet/sbatch_scripts/diffusion_policy/real_robot/to_run/pick_cube.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=robomimic_pick_cube_act_seq_50
-#SBATCH --output=/coc/flash7/nkra3/logs/sbatch_out/phd_project/real_robot/robomimic_pick_cube_act_seq_50.out
-#SBATCH --error=/coc/flash7/nkra3/logs/sbatch_err/phd_project/real_robot/robomimic_pick_cube_act_seq_50.err
+#SBATCH --job-name=robomimic_pick_cube_transformer
+#SBATCH --output=/coc/flash7/nkra3/logs/sbatch_out/phd_project/real_robot/robomimic_pick_cube_transformer.out
+#SBATCH --error=/coc/flash7/nkra3/logs/sbatch_err/phd_project/real_robot/robomimic_pick_cube_transformer.err
 #SBATCH --partition=overcap
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
-#SBATCH --gpus-per-node="titan_x:1"
+#SBATCH --gpus-per-node="a40:1"
 #SBATCH --exclude="clippy"
 #SBATCH --mem-per-gpu=64G
 
@@ -20,7 +20,7 @@ conda activate robomimic-dev
 cd /nethome/nkra3/flash7/phd_project/robomimic-nadun/robomimic
 
 config_folder=/nethome/nkra3/flash7/phd_project/robomimic-nadun/skynet/configs/diffusion-policy/real_robot/pick_cube/
-config="pick_cube_act.json"
+config="pick_cube_transformer.json"
 config_path="$config_folder$config"
 
 srun -u python -u scripts/train.py --config=$config_path
