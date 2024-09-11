ab9b1e82d6219395ce1a916ba04d54cb51e7f50f
Nadun Kesara Ranawaka Arachchige <nkra3@sky1.cc.gatech.edu>
2024-09-11 01:24:15 -0400

    Started training of absolute action models

--- robomimic/dev/add_all_actions.py
+++ robomimic/dev/add_all_actions.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import robomimic.utils.transform_utils as T
 from copy import deepcopy
+import argparse
 
 def delta_axis_angle_to_absolute(delta_angles, quats):
     ret = []
@@ -23,70 +24,87 @@ def delta_axis_angle_to_absolute(delta_angles, quats):
 
     return np.array(ret)
 
+def add_actions_to_dataset(demo_fn):
 
-demo_fn  = "/media/nadun/Data/phd_project/robomimic/datasets/square/ph/low_dim_v141.hdf5"
-# demo = nx.nxload(demo_fn)
-# print(demo.tree)
+    print(f"Processing dataset: {demo_fn}")
 
-demo_f = h5py.File(demo_fn, "a")
-demo_file = demo_f['data']
-env_args = demo_file.attrs["env_args"]
-print(env_args)
-counter = 0
+    # demo_fn  = "/media/nadun/Data/phd_project/robomimic/datasets/square/ph/low_dim_v141.hdf5"
+    # demo = nx.nxload(demo_fn)
+    # print(demo.tree)
 
-for demo in demo_file:
-    if (counter % 10) == 0:
-        print(f"Processing demo: {counter}")
-    counter += 1
-    demo = demo_file[demo]
+    demo_f = h5py.File(demo_fn, "a")
+    demo_file = demo_f['data']
+    env_args = demo_file.attrs["env_args"]
+    # print(env_args)
+    counter = 0
 
-    ### Cleanup everything first
-    if "absolute_actions" in demo:
-        del demo["absolute_actions"]
-    if "joint_actions" in demo:
-        del demo["joint_actions"]
-    if "joint_position_actions" in demo:
-        del demo["joint_position_actions"]
-    if "delta_joint_actions" in demo:
-        del demo["delta_joint_actions"]
+    for demo in demo_file:
+        if (counter % 10) == 0:
+            print(f"Processing demo: {counter}")
+        counter += 1
+        demo = demo_file[demo]
 
-    ### Create absolute eef actions
-    delta_actions = demo['actions'][:]
+        ### Cleanup everything first
+        if "absolute_actions" in demo:
+            del demo["absolute_actions"]
+        if "joint_actions" in demo:
+            del demo["joint_actions"]
+        if "joint_position_actions" in demo:
+            del demo["joint_position_actions"]
+        if "delta_joint_actions" in demo:
+            del demo["delta_joint_actions"]
 
-    # Create absolute position action
-    delta_pos = delta_actions[:, 0:3]
-    pos = demo['obs/robot0_eef_pos'][:]
-    target_pos = delta_pos + pos
+        ### Create absolute eef actions
+        delta_actions = demo['actions'][:]
 
-    # Create absolute axis angle rotation action
-    delta_aa = delta_actions[:, 3:6]
-    quat = demo['obs/robot0_eef_quat'][:]
-    target_axis_angles = delta_axis_angle_to_absolute(delta_aa, quat)
+        # Create absolute position action
+        delta_pos = delta_actions[:, 0:3]
+        pos = demo['obs/robot0_eef_pos'][:]
+        target_pos = delta_pos + pos
 
-    # Create new absolute action array:
-    gripper_act = delta_actions[:, -1, np.newaxis]
-    absolute_actions = np.concatenate((target_pos, target_axis_angles, gripper_act), axis=1)
-    demo.create_dataset("absolute_actions", data=absolute_actions)
+        # Create absolute axis angle rotation action
+        delta_aa = delta_actions[:, 3:6]
+        quat = demo['obs/robot0_eef_quat'][:]
+        target_axis_angles = delta_axis_angle_to_absolute(delta_aa, quat)
 
-    ### Create joint position action
-    joint_actions = demo['obs/robot0_joint_pos'][1:] # We offset by 1 since the next joint position is the action
-    last_joint_action = np.copy(demo['obs/robot0_joint_pos'][-1:, :])
-    joint_actions = np.concatenate([joint_actions, last_joint_action], axis=0)
+        # Create new absolute action array:
+        gripper_act = delta_actions[:, -1, np.newaxis]
+        absolute_actions = np.concatenate((target_pos, target_axis_angles, gripper_act), axis=1)
+        demo.create_dataset("absolute_actions", data=absolute_actions)
 
-    # Create new joint action array with the gripper action included:
-    gripper_act = delta_actions[:, -1, np.newaxis]
-    joint_actions = np.concatenate((joint_actions, gripper_act), axis=1)
-    demo.create_dataset("joint_position_actions", data=joint_actions)
+        ### Create joint position action
+        joint_actions = demo['obs/robot0_joint_pos'][1:] # We offset by 1 since the next joint position is the action
+        last_joint_action = np.copy(demo['obs/robot0_joint_pos'][-1:, :])
+        joint_actions = np.concatenate([joint_actions, last_joint_action], axis=0)
 
-    # Creating delta joint actions
-    delta_joint_actions = deepcopy(demo['joint_position_actions'][:])
+        # Create new joint action array with the gripper action included:
+        gripper_act = delta_actions[:, -1, np.newaxis]
+        joint_actions = np.concatenate((joint_actions, gripper_act), axis=1)
+        demo.create_dataset("joint_position_actions", data=joint_actions)
 
-    joint_obs = demo['obs/robot0_joint_pos'][:]
-    delta_joint_actions[:, :-1] = delta_joint_actions[:, :-1] - joint_obs
-    demo.create_dataset("delta_joint_actions", data=delta_joint_actions)
+        # Creating delta joint actions
+        delta_joint_actions = deepcopy(demo['joint_position_actions'][:])
+
+        joint_obs = demo['obs/robot0_joint_pos'][:]
+        delta_joint_actions[:, :-1] = delta_joint_actions[:, :-1] - joint_obs
+        demo.create_dataset("delta_joint_actions", data=delta_joint_actions)
+
+    demo_f.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    # Path to trained model
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        required=True,
+        help="path to dataset",
+    )
+    args = parser.parse_args()
 
 
-demo_f.close()
+    add_actions_to_dataset(args.dataset)
 
 
 
